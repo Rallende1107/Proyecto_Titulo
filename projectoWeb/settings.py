e3dc3b532dd12428ce99969122959b28a614544d
Rene Allende <52171906+Rallende1107@users.noreply.github.com>
2024-06-21 12:28:14 -0400

    dj-database-url

--- projectoWeb/settings.py
+++ projectoWeb/settings.py
@@ -96,12 +96,32 @@ WSGI_APPLICATION = 'projectoWeb.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+ENV = 'PROD'
+
+if ENV == 'DEV':
+    DATABASES = {
+        'default': dj_database_url.config(
+            default=os.os.getenv("DATABASE_URL_DEV")
+        )
     }
-}
+elif ENV == 'DEV':
+    DATABASES = {
+        'default': dj_database_url.config(
+            default=os.os.getenv("DATABASE_URL")
+        )
+    }
+
+
+
+
+
 
 # DATABASES = {
 #    'default': {
