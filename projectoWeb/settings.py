c3b2ef887bd6217764e428ea6dd4b19c0f5a3da5
Rene Allende <52171906+Rallende1107@users.noreply.github.com>
2024-06-21 12:13:48 -0400

    Update settings.py

--- projectoWeb/settings.py
+++ projectoWeb/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 DEBUG = "True"
 
 # ALLOWED_HOSTS = []
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '[::1]', '0.0.0.0', 'proyectotitulo-production-1f3a.up.railway.app', 'vecina-hay-pan.cl']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '[::1]', '0.0.0.0', 'https://proyectotitulo-production-1f3a.up.railway.app', 'vecina-hay-pan.cl']
 
 
 # Application definition
