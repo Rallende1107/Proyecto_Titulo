38dae5483a9830bf0a0b874364eaf066344a6869
gaby <104112189+Gcabrerameneses@users.noreply.github.com>
2024-06-20 20:16:16 -0400

    fixs css
    fixs css

--- projectoWeb/settings.py
+++ projectoWeb/settings.py
@@ -22,11 +22,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv("SECRET_KEY")
-
+# SECRET_KEY = os.getenv("SECRET_KEY")
+SECRET_KEY = "django-insecure-r7)07i8v6)abla1!ofwt^n!0eenrghujgpb7@#(b4ct%2_+@bn"
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv("DEBUG") == "True"
-
+# DEBUG = os.getenv("DEBUG") == "True"
+DEBUG = "True"
 
 # ALLOWED_HOSTS = []
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', '[::1]', '0.0.0.0']
