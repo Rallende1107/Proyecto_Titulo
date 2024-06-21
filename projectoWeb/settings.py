569b74bd9ffe5ff97410887dd965e83e8c2ee1ff
Rene Allende <52171906+Rallende1107@users.noreply.github.com>
2024-06-21 12:09:56 -0400

    Update settings.py

--- projectoWeb/settings.py
+++ projectoWeb/settings.py
@@ -22,8 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = os.getenv("SECRET_KEY")
-SECRET_KEY = "django-insecure-r7)07i8v6)abla1!ofwt^n!0eenrghujgpb7@#(b4ct%2_+@bn"
+SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = os.getenv("DEBUG") == "True"
 DEBUG = "True"
